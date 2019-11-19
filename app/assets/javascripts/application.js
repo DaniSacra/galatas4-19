bd90decb433ce381a39a8fbc00f19f84087a2ae7
danisacra <danisacra@gmail.com>
2019-11-19 14:31:05 -0300

    Adding player

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 
 //= require jquery/dist/jquery.min
 //= require bootstrap/dist/js/bootstrap.bundle.min
-//= require clean-blog.min.js
+//= require clean-blog.min
 
 //= require_tree .
--- app/views/episodes/episodes/_episode.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! episode, :id, :title, :description, :launch, :created_at, :updated_at
-json.url episode_url(episode, format: :json)
